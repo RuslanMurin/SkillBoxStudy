fe98ab4ff209181ffed992a4c8e39b57e36c55d1
Ruslan Murin <rslnmurin@gmail.com>
2020-10-28 22:44:45 +0300

    Revert "Правка"
    This reverts commit dace444e87b2b6f067bd414e37717d24e00c39fa.

--- 13.0-Animations/13.0-Animations/ViewController.swift
+++ 13.0-Animations/13.0-Animations/ViewController.swift
@@ -1,49 +1,11 @@
 import UIKit
 
-enum Animation: String {
-    case background = "1"
-    case move = "2"
-    case rounding = "3"
-    case rotate = "4"
-    case alpha = "5"
-    case size = "6"
-    case repeating = "7"
-    
-    mutating func previous() {
-        switch self {
-        case .background:
-            self = .background
-        case .move:
-            self = .background
-        case .rounding:
-            self = .move
-        case .rotate:
-            self = .rounding
-        case .alpha:
-            self = .rotate
-        case .size:
-            self = .alpha
-        case .repeating:
-            self = .size
-        }
-    }
-    
-    mutating func next() {
-        switch self {
-        case .background:
-            self = .move
-        case .move:
-            self = .rounding
-        case .rounding:
-            self = .rotate
-        case .rotate:
-            self = .alpha
-        case .alpha:
-            self = .size
-        case .size:
-            self = .repeating
-        case .repeating:
-            self = . repeating
+class Actions{
+    let actionsArray: [String] = ["1","2","3","4","5","6","7"]
+    var currentIndex: Int = 0{
+        didSet{
+            if currentIndex < 0 { currentIndex = 0 }
+            else if currentIndex > 6 { currentIndex = 6 }
         }
     }
 }
@@ -54,83 +16,83 @@ class ViewController: UIViewController {
     @IBOutlet weak var squareTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var squareCenterHorisontalyConstraint: NSLayoutConstraint!
     
-    var currentAnimation = Animation.background
+    let action = Actions()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        numberLabel.text = currentAnimation.rawValue
+        numberLabel.text = action.actionsArray[action.currentIndex]
     }
     
     
     @IBAction func previousTapped(_ sender: UIButton) {
-        currentAnimation.previous()
-        animate()
+        action.currentIndex -= 1
+        numberLabel.text = action.actionsArray[action.currentIndex]
+        animate(actionIndex: action.currentIndex)
+        print(action.currentIndex)
     }
     
     @IBAction func nextTapped(_ sender: Any) {
-        currentAnimation.next()
-        animate()
+        action.currentIndex += 1
+        numberLabel.text = action.actionsArray[action.currentIndex]
+        animate(actionIndex: action.currentIndex)
+        print(action.currentIndex)
     }
     
-    func animate() {
-        squareView.layer.removeAllAnimations()
-        numberLabel.text = currentAnimation.rawValue
-        let animator = UIViewPropertyAnimator(duration: 1.0, curve: .linear)
-        let reverseAnimator = UIViewPropertyAnimator(duration: 1.0, curve: .linear)
-        switch currentAnimation {
-        case .background:
-            animator.addAnimations{
-                self.squareView.backgroundColor = .systemYellow
-            }
-            animator.addCompletion { _ in
-                reverseAnimator.addAnimations{
-                    self.squareView.backgroundColor = .systemRed
-                }
-                reverseAnimator.startAnimation()
-            }
-            animator.startAnimation()
-        case .move:
-            self.squareTopConstraint.constant = 0
-            self.squareCenterHorisontalyConstraint.constant = self.view.frame.size.width / 2 - 50
+    func animate(actionIndex: Int){
+        switch actionIndex {
+        case 0:
+            squareView.layer.removeAllAnimations()
             UIView.animate(withDuration: 2.0, delay: 0, options: .autoreverse, animations: {
+                self.squareView.backgroundColor = .systemYellow
+            }, completion: { (isCompleted) in self.squareView.backgroundColor = .systemRed })
+        case 1:
+            squareView.layer.removeAllAnimations()
+            UIView.animate(withDuration: 2.0, delay: 0, options: .autoreverse, animations: {
+                self.squareTopConstraint.constant = 0
+                self.squareCenterHorisontalyConstraint.constant = self.view.frame.size.width / 2 - 50
                 self.view.layer.layoutIfNeeded()
-            }, completion: { _ in
-                self.squareTopConstraint.constant = 80
+            }, completion: { (isCompleted) in self.squareTopConstraint.constant = 80
                 self.squareCenterHorisontalyConstraint.constant = 0
+                self.view.layer.layoutIfNeeded()
             })
-        case .rounding:
-            animator.addAnimations {
-                self.squareView.layer.cornerRadius = self.squareView.frame.size.width / 2
-            }
-            animator.addCompletion { _ in
-                reverseAnimator.addAnimations{
-                    self.squareView.layer.cornerRadius = 0
-                }
-                reverseAnimator.startAnimation()
-            }
-            animator.startAnimation()
-        case .rotate:
+        case 2:
+            squareView.layer.removeAllAnimations()
+            let animation = CABasicAnimation(keyPath: "cornerRadius")
+            animation.fromValue = NSNumber(value: 0)
+            animation.toValue = NSNumber(value: Int(squareView.frame.size.width) / 2)
+            animation.duration = 2.0
+            animation.autoreverses = true
+            squareView.layer.add(animation, forKey: "cornerRadius")
+            squareView.layer.cornerRadius = 0
+        case 3:
+            squareView.layer.removeAllAnimations()
             UIView.animate(withDuration: 2.0, delay: 0, options: .autoreverse, animations: {
                 self.squareView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-            }, completion: { _ in
+            }, completion: { (isCompleted) in
                 self.squareView.transform = .identity
             })
-        case .alpha:
+        case 4:
+            squareView.layer.removeAllAnimations()
             UIView.animate(withDuration: 2.0, delay: 0, options: .autoreverse, animations: {
                 self.squareView.alpha = 0
-            }, completion: { _ in self.squareView.alpha = 1} )
-        case .size:
+            }, completion: { (isCompleted) in self.squareView.alpha = 1} )
+        case 5:
+            squareView.layer.removeAllAnimations()
             UIView.animate(withDuration: 2.0, delay: 0, options: .autoreverse, animations: {
-                self.squareView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)}, completion: { _ in
-                    self.squareView.transform = .identity
-            })
-        case .repeating:
-            UIView.animate(withDuration: 2.0, delay: 0, options: [.repeat, .curveLinear], animations: {
-                self.squareView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-            }, completion: { _ in
+                self.squareView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)}, completion: { (isCompleted) in
                 self.squareView.transform = .identity
             })
+        case 6:
+            squareView.layer.removeAllAnimations()
+            UIView.animate(withDuration: 2.0, delay: 0, options: [.repeat, .curveLinear], animations: {
+            self.squareView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            }, completion: {(isCompleted) in
+                self.squareView.transform = .identity
+            })
+        default:
+            break
         }
     }
+    
 }
 
